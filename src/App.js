0e2f0b4983fdda466bcf88ceb02af7789cd0c314
smartphp <3238355967@qq.com>
2018-01-20 16:39:29 +0800

    *** 简短说明干了什么**  What: 配置完毕
    ---------------------------------------
    *** 为什么这么做?**
    Why:
    
    ***怎么解决的**
    How:
    
    ---------------------------------------
    
    ``
    
    ---------------------------------------

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
+import { graphql} from 'react-apollo';
+import gql from 'graphql-tag';
+
+const getOneNode = gql`
+  query ($id: String!){
+    OneNode(id:$id) {
+       id,
+       tab,
+       content,
+       
+    }
+  }
+`;
 class App extends Component {
+  
   render() {
+    console.log(this.props.data);
+    if(!this.props.data.loading){
+     var content=this.props.data.OneNode.content;
+     console.log(typeof(content))
+    }else{
+      var content = "";
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+        <div>{ReactHtmlParser(content)}</div>;
+        </div>
+
       </div>
     );
   }
 }
 
-export default App;
+export default graphql(getOneNode, {options: () => {
+  
+  return ({
+    variables: {
+      id: '5433d5e4e737cbe96dcef312',
+     
+    }
+  });
+}})(App)
--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjaxudkum2ugf0127kok921bc' }),
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+});
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
